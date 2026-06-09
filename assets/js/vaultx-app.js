9d3ead2ff031f93db74fa2f2cfb2615e8b63ae4d
Rohan Nayak <rohannayak360@gmail.com>
2026-06-09 12:41:44 +0530

    5th phase

--- assets/js/vaultx-app.js
+++ assets/js/vaultx-app.js
@@ -1,1 +1,19 @@
 // Main controller
+
+const copyBtn =
+document.getElementById("copyBtn");
+
+const toast =
+document.querySelector(".toast");
+
+copyBtn.addEventListener("click",()=>{
+
+    toast.style.display="block";
+
+    setTimeout(()=>{
+
+        toast.style.display="none";
+
+    },2000);
+
+});
